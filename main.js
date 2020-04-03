d6bb40915152691b45f6f8f0a16da03b7fef2234
Akansha Panwar <panwarakansha431@gmail.com>
2020-04-03 22:51:40 +0530

    Hamburger added

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-$(document).ready(function(){
-
-// Adding Footer To DOM
-
- $('footer').append();
-
-// Adding Footer To DOM
-
-
-
-
-
-
-
-
-});
+// ===============================HEADER================================
+function myFunction() {
+    var x = document.getElementById("submenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("123").style.display = "none";
+    
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("123").style.display = "block";
+  }
+// ======================End Of HEADER======================================
