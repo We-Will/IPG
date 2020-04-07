e30931f807da252380a09fde59eb6ecb3c9dc998
Akansha Panwar <panwarakansha431@gmail.com>
2020-04-07 20:27:00 +0530

    Hamburger changes

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 // ===============================HEADER================================
-function myFunction() {
-    var x = document.getElementById("submenu");
+function myFunction_properties() {
+    var x = document.getElementById("submenu_properties");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
   }
+  function myFunction_calculator() {
+    var x = document.getElementById("submenu_calculator");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+ 
   function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("submenu").style.display = "none";
+    document.getElementById("submenu_properties").style.display = "none";
+    document.getElementById("submenu_calculator").style.display = "none";
     document.getElementById("mySidenav").style.width = "0";
 }
 // ======================End Of HEADER======================================
